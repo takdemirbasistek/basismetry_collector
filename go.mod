e1dd383a7838d2b5b9a283baf4bbab492fdc0d5b
Taner Akdemir <taner.akdemir@basistek.com>
2023-10-04 17:05:21 +0300

    First commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/takdemirbasistek/basismetry_collector
+
+go 1.20
---
+++ pkg/collector/collector.go
@@ -0,0 +1,20 @@
+package collector
+
+type Collector struct {
+}
+
+func (c *Collector) StartTrace() {
+
+}
+
+func (c *Collector) EndTrace() {
+
+}
+
+func (c *Collector) StartSpan() {
+
+}
+
+func (c *Collector) EndSpan() {
+
+}
